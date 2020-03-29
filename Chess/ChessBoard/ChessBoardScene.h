4e96f5e08fe9736e283b92e3227aaa4146c9a7d2
Dominik <RzeplinskiDominik@gmail.com>
2020-03-29 21:16:48 +0200

    Add doxygen descriptions part 1/2

--- Chess/ChessBoard/ChessBoardScene.h
+++ Chess/ChessBoard/ChessBoardScene.h
@@ -9,34 +9,122 @@
 #include "ChessBoard.h"
 #include "ChessBoardHistory.h"
 
+/*!
+ * \brief The ChessBoardScene class represents view and controller for game
+ */
 class ChessBoardScene:public QGraphicsScene
 {
 public:
+  /*!
+   * \brief ChessBoardScene constructor
+   * \param parent QObject
+   */
   ChessBoardScene(QObject *parent = 0);
+  /*!
+   * \brief Reset game
+   */
   void Reset();
+  /*!
+   * \brief saveGame save game in file
+   * \param fileName name of file
+   */
   void saveGame(QString fileName);
+  /*!
+   * \brief loadGame from file
+   * \param fileName file name
+   */
   void loadGame(QString fileName);
 private:
+  /*!
+   * \brief boxes list of boxes of chess board
+   */
   QList<ChessBoardBox*> boxes;
+  /*!
+   * \brief panelLeft left panel for player
+   */
   ChessBoardSidePanel* panelLeft;
+  /*!
+   * \brief panelRight right panel for player
+   */
   ChessBoardSidePanel* panelRight;
+  /*!
+   * \brief figureViews views of alive and killed figures in game
+   */
   QList<FigureView*> figureViews;
+  /*!
+   * \brief promotionFigureViews views of promotion figures for pawn pwomotion
+   */
   QList<FigureView*> promotionFigureViews;
+  /*!
+   * \brief promotion promotion box for pawn promotion select
+   */
   ChessBoardPromotion *promotion;
+  /*!
+   * \brief playerTextLeft text container for left player
+   */
   ChessBoardPlayerText *playerTextLeft;
+  /*!
+   * \brief playerTextRight text container for right player
+   */
   ChessBoardPlayerText *playerTextRight;
+  /*!
+   * \brief getBoxAtPosition return box at given coordinates
+   * \param positionX coordinate of box
+   * \param positionY coordinate of box
+   * \return pointer for box if exist, other null
+   */
   ChessBoardBox* getBoxAtPosition(int positionX, int positionY);
+  /*!
+   * \brief getBoxUnderMouse retrive box under mouse position
+   * \return pointer for box under mouse cursor if exist, other null
+   */
   ChessBoardBox* getBoxUnderMouse();
+  /*!
+   * \brief setColorForBoxes Update colors of boxes
+   */
   void setColorForBoxes();
+  /*!
+   * \brief cleanScene remove all iteams from scene
+   */
   void cleanScene();
+  /*!
+   * \brief chessBoard pointer for chess board model
+   */
   ChessBoard* chessBoard;
+  /*!
+   * \brief history pointer for history of game
+   */
   ChessBoardHistory* history;
+  /*!
+   * \brief refresh views
+   */
   void refresh();
+  /*!
+   * \brief refreshAfterPromotion refresh views of promotion
+   */
   void refreshAfterPromotion();
 public slots:
+  /*!
+   * \brief promotionSelected Slot for selecting figure for pawn promotion
+   * \param positionX coordinates of selected figure
+   * \param positionY coordinates of selected figure
+   */
   void promotionSelected(int positionX, int positionY);
+  /*!
+   * \brief validMoves for selected figure
+   * \param positionX coordinates of selected figure
+   * \param positionY coordinates of selected figure
+   */
   void validMoves(int positionX, int positionY);
+  /*!
+   * \brief clearMoves remove moves from view
+   */
   void clearMoves();
+  /*!
+   * \brief setNewPosition set new position for figure
+   * \param positionX coordinates of figure
+   * \param positionY coordinates of figure
+   */
   void setNewPosition(int positionX, int positionY);
 };
 
--- Chess/mainwindow.h
+++ Chess/mainwindow.h
@@ -8,25 +8,53 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+/*!
+ * \brief The MainWindow class
+ */
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
 public:
+  /*!
+   * \brief MainWindow constructor
+   * \param parent QWidget
+   */
   MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
 
 private slots:
+  /*!
+   * \brief on_actionExit_triggered slot for menu exit action
+   */
   void on_actionExit_triggered();
 
+  /*!
+   * \brief on_actionNew_game_triggered slot for meny new game action
+   */
   void on_actionNew_game_triggered();
 
+  /*!
+   * \brief on_actionSave_game_triggered slot for save game action
+   */
   void on_actionSave_game_triggered();
   
+  /*!
+   * \brief on_actionLoad_game_triggered slot for load game action
+   */
   void on_actionLoad_game_triggered();
+  /*!
+   * \brief Reset game
+   */
   void Reset();
 private:
+  /*!
+   * \brief ui
+   */
   Ui::MainWindow *ui;
+  /*!
+   * \brief m_sceneChessBoard
+   */
   ChessBoardScene *m_sceneChessBoard;
 };
 #endif // MAINWINDOW_H
